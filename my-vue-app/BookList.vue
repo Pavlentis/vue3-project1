e4323370ef264347190486b1c19776532646300c
Pavlentis <pmierkushiev@mail.ru>
2024-12-05 09:01:15 +0000

    edit1

---
+++ my-vue-app/BookList.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul>
+      <li v-for="book in books" :key="book.id">
+        {{ book.title }} ({{ book.author }})
+        <button @click="$emit('edit', book)">Редактировать</button>
+        <button @click="$emit('delete', book.id)">Удалить</button>
+      </li>
+    </ul>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      books: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
