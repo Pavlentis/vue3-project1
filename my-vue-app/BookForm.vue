3f2610c5812e00a8af2e7d391cf11635c38d5957
Pavlentis <pmierkushiev@mail.ru>
2024-12-05 08:42:19 +0000

    Initial commit: edit  Vue 3 project

---
+++ my-vue-app/BookForm.vue
@@ -0,0 +1,40 @@
+<template>
+    <form @submit.prevent="submitForm">
+      <input v-model="book.title" type="text" placeholder="Название книги">
+      <input v-model="book.author" type="text" placeholder="Автор">
+      <button type="submit">Сохранить</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        book: {
+          id: null,
+          title: '',
+          author: '',
+        },
+      };
+    },
+    methods: {
+      submitForm() {
+        if (this.book.id) {
+          // Редактирование книги
+          this.$emit('edit', this.book);
+        } else {
+          // Добавление новой книги
+          this.$emit('add', this.book);
+        }
+        this.resetForm();
+      },
+      resetForm() {
+        this.book = {
+          id: null,
+          title: '',
+          author: '',
+        };
+      },
+    },
+  };
+  </script>
